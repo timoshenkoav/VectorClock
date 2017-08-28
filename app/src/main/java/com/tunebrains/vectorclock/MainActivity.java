3bf43c3ae9054345087e80ffe2363fd6402bf59c
Alexandr Timoshenko <thick.tav@gmail.com>
2017-08-28 21:06:58 +0200

    third v view

--- app/src/main/java/com/tunebrains/vectorclock/MainActivity.java
+++ app/src/main/java/com/tunebrains/vectorclock/MainActivity.java
@@ -2,12 +2,48 @@ package com.tunebrains.vectorclock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.tunebrains.vectorclocklib.VectorDigitalClock;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    long startTime;
+    private VectorDigitalClock clock;
+
+    ScheduledExecutorService executorService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clock = (VectorDigitalClock) findViewById(R.id.vector_digital_clock);
+        startTime = System.currentTimeMillis();
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                startTime += TimeUnit.MINUTES.toMillis(1);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        clock.updateTime(startTime);
+                    }
+                });
+            }
+        },2,2, TimeUnit.SECONDS);
+    }
+
+    @Override
+    protected void onPause() {
+        executorService.shutdownNow();
+        super.onPause();
     }
 }
